422ebd852a8e1cceb80ba736709522e976c94244
ShuvoAsif <asif.shuvo.learning@gmail.com>
2022-10-28 00:17:42 +0600

    firebase config

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAHh-Asm2lLESMnse-_9NUmnDgWVyzSu7M",
+    authDomain: "course-academy-73981.firebaseapp.com",
+    projectId: "course-academy-73981",
+    storageBucket: "course-academy-73981.appspot.com",
+    messagingSenderId: "996652181082",
+    appId: "1:996652181082:web:9021bf76bf3217c66dda63"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
