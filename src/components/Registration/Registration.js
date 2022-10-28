87272919c544d849955b3b7a37c3f7e726c170c5
ShuvoAsif <asif.shuvo.learning@gmail.com>
2022-10-28 15:21:39 +0600

    AuthProvider

---
+++ src/components/Registration/Registration.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Registration = () => {
+    return (
+        <div>
+            <h1>this is Registration</h1>
+        </div>
+    );
+};
+
+export default Registration;
