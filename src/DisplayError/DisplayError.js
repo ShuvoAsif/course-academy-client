71e225abf391ad42c91c558dadc347023f4efb36
ShuvoAsif <asif.shuvo.learning@gmail.com>
2022-12-22 10:03:37 +0600

    updated

--- src/DisplayError/DisplayError.js
+++ src/DisplayError/DisplayError.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import { useRouteError } from 'react-router-dom';
 import img from '../images/errorpage.jpg'
 
 const DisplayError = () => {
-    const error = useRouteError();
 
     return (
-        <div className='px-10 bg-zinc-100'>
-            <p className='text-red-500 font-bold text-center'>Something went wrong!!!</p>
-            <p className='text-red-400 text-center'>{error.statusText || error.message}</p>
-            <img src={img} alt="" />
+        <div >
+            <img src={img} alt="" className="w-75" />
         </div>
     );
 };
